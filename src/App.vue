5a096402e615c1cbf5c7c99bac59d8390b95606b
Karan Parmar <karan@solvative.com>
2022-08-30 00:02:11 +0530

    Initial Setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-app-primary text-white">
+    <div class="p-5">Hello World !</div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "app-primary": "#00668A",
+                "app-secondary": "#004E71",
+            },
+            fontFamily: {
+                Roboto: ["Roboto", "sans-serif"],
+            },
+            container: {
+                padding: "2rem",
+                center: true,
+            },
+            screens: {
+                sm: "640px",
+                md: "768px",
+            },
+        },
+    },
+};
